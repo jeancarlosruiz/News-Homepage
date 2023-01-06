868eba23172f7f28d1a1e8fcb2ab316871981448
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-01-06 01:19:12 -0400

    Final-project v-4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import htmlPurge from "vite-plugin-html-purgecss";
+import htmlPurge from 'vite-plugin-html-purgecss';
 
 export default {
   plugins: [htmlPurge()],
+  base: '/News-Homepage/',
 };
 
 // npm i vite-plugin-html-purgecss -D
